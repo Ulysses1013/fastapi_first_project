43b8ac7b61f8b72699cb6cc244b55d4361b75b5a
Ulysses1013 <kanaina99102@gmail.com>
2021-12-29 16:43:03 +0900

    fixed test

--- sql_app/test_main.py
+++ sql_app/test_main.py
@@ -19,13 +19,13 @@ def test_read_bookings():
     assert response.json() != None
 
 def test_create_users():
-    response = client.post("/users", json={"username":"福永"})
+    response = client.post("/users", json={"username":"坂井"})
 
     assert response.status_code == 200
     assert response.json() != None
 
 def test_create_rooms():
-    response = client.post("/rooms", json={"room_name":"1on1面談室","capacity":2})
+    response = client.post("/rooms", json={"room_name":"大会議室","capacity":12})
 
     assert response.status_code == 200
     assert response.json() != None
@@ -34,11 +34,11 @@ def test_create_bookings():
     response = client.post(
         "/bookings",
         json={
-            "room_id": 1,
+            "room_id": 3,
             "user_id": 1,
-            "booked_num": 5,
-            "start_datetime": "2021-12-25T08:00:41.375Z",
-            "end_datetime": "2021-12-25T08:30:39.394Z"
+            "booked_num": 6,
+            "start_datetime": "2021-12-29T07:15:33.592Z",
+            "end_datetime": "2021-12-29T07:45:33.592Z"
         },
     )
     assert response.status_code == 200
@@ -47,11 +47,11 @@ def test_create_bookings():
     response = client.post(
         "/bookings",
         json={
-            "room_id": 1,
+            "room_id": 3,
             "user_id": 2,
-            "booked_num": 3,
-            "start_datetime": "2021-12-25T08:15:41.375Z",
-            "end_datetime": "2021-12-25T08:45:39.394Z"
+            "booked_num": 10,
+            "start_datetime": "2021-12-29T07:00:33.592Z",
+            "end_datetime": "2021-12-29T07:30:33.592Z"
         },
     )
     assert response.status_code == 404
